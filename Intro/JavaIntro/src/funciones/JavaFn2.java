6f040a70f07f5dfdc0620fa52b8da6dcbf14c5b9
Stefany Cantero <stefanycanteroc@gmail.com>
2023-06-05 19:35:09 -0500

    Fns 2

---
+++ Intro/JavaIntro/src/funciones/JavaFn2.java
@@ -0,0 +1,23 @@
+/*
+Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
+muestre por pantalla en orden descendente.
+ */
+package funciones;
+/**
+ *
+ * @author Stefany
+ */
+public class JavaFn2 {
+    public static void main(String[] args) {
+        int[] vector = new int[100];
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = vector.length - i;
+        }
+
+        System.out.println("Vector en orden descendente:");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i]);
+        }
+    }
+}
