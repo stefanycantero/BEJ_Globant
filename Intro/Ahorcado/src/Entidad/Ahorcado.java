37c3e57e6d8d95e35fefe3730a767598ca4cb55f
Stefany Cantero <stefanycanteroc@gmail.com>
2023-07-11 18:27:16 -0500

    Ahorcado 2

--- Intro/Ahorcado/src/Entidad/Ahorcado.java
+++ Intro/Ahorcado/src/Entidad/Ahorcado.java
@@ -5,5 +5,42 @@ package Entidad;
  * @author Stefany
  */
 public class Ahorcado {
+    private String[] palabraBuscada;
+    private int cantidadLetrasEncontradas;
+    private int jugadasMaximas;
+
+    public Ahorcado() {
+    }
+
+    public Ahorcado(String[] palabraBuscada, int cantidadLetrasEncontradas, int jugadasMaximas) {
+        this.palabraBuscada = palabraBuscada;
+        this.cantidadLetrasEncontradas = cantidadLetrasEncontradas;
+        this.jugadasMaximas = jugadasMaximas;
+    }
+
+    public String[] getPalabraBuscada() {
+        return palabraBuscada;
+    }
+
+    public void setPalabraBuscada(String[] palabraBuscada) {
+        this.palabraBuscada = palabraBuscada;
+    }
+
+    public int getCantidadLetrasEncontradas() {
+        return cantidadLetrasEncontradas;
+    }
+
+    public void setCantidadLetrasEncontradas(int cantidadLetrasEncontradas) {
+        this.cantidadLetrasEncontradas = cantidadLetrasEncontradas;
+    }
+
+    public int getJugadasMaximas() {
+        return jugadasMaximas;
+    }
+
+    public void setJugadasMaximas(int jugadasMaximas) {
+        this.jugadasMaximas = jugadasMaximas;
+    }
+    
     
 }
--- Intro/Ahorcado/src/Servicio/ServicioAhorcado.java
+++ Intro/Ahorcado/src/Servicio/ServicioAhorcado.java
@@ -1,9 +1,112 @@
 package Servicio;
 
+import Entidad.Ahorcado;
+import java.util.Scanner;
+
 /**
  *
  * @author Stefany
  */
 public class ServicioAhorcado {
-    
+
+    Scanner sc = new Scanner(System.in);
+    Ahorcado ahorcado = new Ahorcado();
+
+    public Ahorcado crearJuego() {
+
+        int jugadasMaximas, letrasEncontradas;
+
+        ahorcado.setPalabraBuscada(ingresarPalabra());
+        System.out.println("Ingresar la cantidad de jugadas máximas: ");
+
+        jugadasMaximas = sc.nextInt();
+        ahorcado.setJugadasMaximas(jugadasMaximas);
+
+        letrasEncontradas = 0;
+        ahorcado.setCantidadLetrasEncontradas(letrasEncontradas);
+
+        return ahorcado;
+    }
+
+    public String[] ingresarPalabra() {
+        String palabra;
+
+        System.out.println("Ingrese una palabra:");
+        palabra = sc.nextLine();
+
+        String[] palabraBuscar = new String[palabra.length()];
+
+        for (int i = 0; i < palabraBuscar.length; i++) {
+            palabraBuscar[i] = palabra.substring(i, i);
+        }
+
+        return palabraBuscar;
+    }
+
+    public void longitud() {
+        System.out.println("Longitud de la palabra: " + ahorcado.getPalabraBuscada().length);
+    }
+
+    public boolean buscar(String letra) {
+
+        boolean esParte = false;
+
+        for (String palabraBuscada : ahorcado.getPalabraBuscada()) {
+            if (palabraBuscada.equals(letra)) {
+                esParte = true;
+            }
+        }
+
+        return esParte;
+
+    }
+
+    public void encontradas(String letra) {
+
+        int cantidadLetrasEncontradas = 0;
+        int cantidadLetrasRestantes = ahorcado.getPalabraBuscada().length - cantidadLetrasEncontradas;
+        int jugadasMaximas = 0;
+
+        for (String palabraBuscada : ahorcado.getPalabraBuscada()) {
+            if (palabraBuscada.equals(letra)) {
+                cantidadLetrasEncontradas++;
+            }
+        }
+
+        System.out.printf("Número de letras (encontradas,faltantes): (%d,%d)", cantidadLetrasEncontradas, cantidadLetrasRestantes);
+
+        if (!buscar(letra)) {
+            jugadasMaximas = ahorcado.getJugadasMaximas() - 1;
+            ahorcado.setJugadasMaximas(jugadasMaximas);
+        }
+
+    }
+
+    public void intentos() {
+        System.out.printf("Número de oportunidades restantes: %d", ahorcado.getJugadasMaximas());
+    }
+
+    public void juego() {
+        String letra;
+
+        crearJuego();
+
+        longitud();
+        
+        System.out.println("Ingrese una letra: ");
+        letra = sc.next();
+        buscar(letra);
+
+        if (buscar(letra)) {
+            System.out.println("Mensaje: la letra pertenece a la palabra");
+        } else {
+            System.out.println("Mensaje: La letra no pertenece a la palabra");
+        }
+        
+        encontradas(letra);
+        
+        intentos();
+
+    }
+
 }
